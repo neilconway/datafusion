50d0ef8ef2abc5197b5d9f7d275dda40dfdd046b
Neil Conway <neil.conway@gmail.com>
2026-04-14 10:24:15 -0400

    Revert clippy benchmark fix

--- benchmarks/src/bin/dfbench.rs
+++ benchmarks/src/bin/dfbench.rs
@@ -20,13 +20,16 @@ use datafusion::error::Result;
 
 use clap::{Parser, Subcommand};
 
+#[cfg(all(feature = "snmalloc", feature = "mimalloc"))]
+compile_error!(
+    "feature \"snmalloc\" and feature \"mimalloc\" cannot be enabled at the same time"
+);
+
 #[cfg(feature = "snmalloc")]
 #[global_allocator]
 static ALLOC: snmalloc_rs::SnMalloc = snmalloc_rs::SnMalloc;
 
-// `cargo clippy --all-features` enables both allocator features; prefer
-// `snmalloc` in that case so the benchmark binaries still compile.
-#[cfg(all(not(feature = "snmalloc"), feature = "mimalloc"))]
+#[cfg(feature = "mimalloc")]
 #[global_allocator]
 static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
